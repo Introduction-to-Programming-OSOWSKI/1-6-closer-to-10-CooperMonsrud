89863f97aa81a19fb8a5cdc4330d13549318fccf
Monsrud Cooper <817455@stma.k12.mn.us>
2021-04-01 10:15:59 -0500

    closerTo10Try2

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def close10(x,y):
-    if (10 - abs(x) < 10 - abs(y)):
-        return x
-    elif (10 - abs(x) > 10 - (y)):
-        return y
+    if abs(10 - x) < abs(10 - y):
+        return (x)
+    elif abs(10 - x) > abs(10 - y):
+        return (0)
+    else:
+        return(y)
+
+print(close10(5,12))
